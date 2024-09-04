e1f08c4f076d1fd02618a6076a77e056edd35a76
Sergey <karakoskinsergej@gmail.com>
2024-09-04 21:48:22 +0300

    getting a random hero in RandomChar component

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,8 @@
+const _api = process.env.REACT_APP_MY_API_KEY;
+
 export default class MarvelService {
-    constructor(api) {
-        this._apiKey = api;
+    constructor() {
+        this._apiKey = _api;
     }
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
 
@@ -14,11 +16,41 @@ export default class MarvelService {
         return await result.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&apikey=${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&apikey=${this._apiKey}`);
+        return this._transformCharacter(res.data.results);
     }
 
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?apikey=${this._apiKey}`);
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?apikey=${this._apiKey}`);
+        return this._transformCharacter(res.data.results);
+    }
+
+    _transformCharacter = (data) => {
+        if (data.length === 1) {
+            return {
+                name: data[0].name ? data[0].name : 'Герой не найден',
+
+                description: !data[0].description ? 
+                                'К сожалению, описание героя отсутствует': 
+                                data[0].description.length > 60 ? 
+                                        `${data[0].description.slice(0, 60)}...` : data[0].description,
+
+                thumbnail: `${data[0].thumbnail.path}.${data[0].thumbnail.extension}`,
+                homepage: data[0].urls[0].url,
+                wiki: data[0].urls[1].url
+            }
+        } else {
+            return data.map(el => {
+                return {
+                    name: el.name ? el.name : 'Герой не найден',
+                    description: el.description ? el.description : 'К сожалению, описание героя отсутствует',
+                    thumbnail: `${el.thumbnail.path}.${el.thumbnail.extension}`,
+                    homepage: el.urls[0].url,
+                    wiki: el.urls[1].url
+                }
+            })
+        }
+        
     }
 }
